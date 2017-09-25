1b189d4b18eb8f78f10583c9b46f7d0172249952
Daniel Amador <dani.amvi@gmail.com>
2017-09-25 20:40:13 +0200

    Fragment creation

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.android.sunshine;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+            // Required empty public constructor
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+            List<String> fakeData = new ArrayList<String>();
+            fakeData.add("Today - Sunny - 30/10");
+            fakeData.add("Tomorrow - Cloudy - 25/8");
+
+            return root;
+
+        }
     }
 }
